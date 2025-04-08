52c645e9d5e795632bb493901584bc3fdfe30ab4
ankita kundliya <ankita2kundliya@gmail.com>
2025-04-08 20:48:29 +0530

    initialfinal1

--- extension/background.js
+++ extension/background.js
@@ -1,31 +1,55 @@
+// let currentTab = "";
+// let startTime = null;
+// let totalTime = {};
+
+// chrome.tabs.onActivated.addListener(async (activeInfo) => {
+//   await chrome.tabs.get(activeInfo.tabId, (tab) => handleSwitch(tab.url));
+// });
+
+// chrome.webNavigation.onCompleted.addListener((details) => {
+//   chrome.tabs.get(details.tabId, (tab) => handleSwitch(tab.url));
+// });
+
+// function handleSwitch(url) {
+//   const domain = new URL(url).hostname;
+
+//   if (currentTab && startTime) {
+//     const diff = Math.floor((Date.now() - startTime) / 1000);
+//     totalTime[currentTab] = (totalTime[currentTab] || 0) + diff;
+
+//     sendToBackend(currentTab, diff);
+//   }
+
+//   currentTab = domain;
+//   startTime = Date.now();
+// }
+
+// function sendToBackend(domain, seconds) {
+//   fetch("https://chromeextension-5g64.onrender.com/api/report", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ domain, seconds }),
+//   });
+// }
 let currentTab = "";
 let startTime = null;
-let totalTime = {};
 
 chrome.tabs.onActivated.addListener(async (activeInfo) => {
-  await chrome.tabs.get(activeInfo.tabId, (tab) => handleSwitch(tab.url));
-});
-
-chrome.webNavigation.onCompleted.addListener((details) => {
-  chrome.tabs.get(details.tabId, (tab) => handleSwitch(tab.url));
-});
-
-function handleSwitch(url) {
-  const domain = new URL(url).hostname;
-
-  if (currentTab && startTime) {
-    const diff = Math.floor((Date.now() - startTime) / 1000);
-    totalTime[currentTab] = (totalTime[currentTab] || 0) + diff;
-
-    sendToBackend(currentTab, diff);
+  if (startTime && currentTab) {
+    const timeSpent = Math.floor((Date.now() - startTime) / 1000);
+    await logTime(currentTab, timeSpent);
   }
 
-  currentTab = domain;
-  startTime = Date.now();
-}
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    if (tab.url) {
+      currentTab = new URL(tab.url).hostname;
+      startTime = Date.now();
+    }
+  });
+});
 
-function sendToBackend(domain, seconds) {
-  fetch("https://chromeextension-5g64.onrender.com/api/report", {
+async function logTime(domain, seconds) {
+  await fetch("https://chromeextension-5g64.onrender.com/api/report", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ domain, seconds }),
--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,8 @@
-document.getElementById("view-report").onclick = () => {
-    chrome.tabs.create({ url: "https://chromeextension-5g64.onrender.com/report" });
-  };
-  
+// document.getElementById("view-report").onclick = () => {
+//     chrome.tabs.create({ url: "https://chromeextension-5g64.onrender.com/report" });
+//   };
+document.getElementById("view-report").addEventListener("click", () => {
+  chrome.tabs.create({
+    url: "https://chromeextension-5g64.onrender.com/report"
+  });
+});
